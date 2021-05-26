4bc843cf4b6ed18778be27653a545dce0d1568ed
Jonathan Jensen <jpjensen@utah.gov>
2021-05-26 16:34:24 -0600

    Starting with flexbox

--- src/app/demo/demo.module.ts
+++ src/app/demo/demo.module.ts
@@ -4,16 +4,20 @@ import {CommonModule} from '@angular/common';
 import {DemoRoutingModule} from './demo-routing.module';
 import {ButtonsComponent} from './buttons/buttons.component';
 import {MaterialModule} from '../shared/material.module';
+import {FlexboxComponent} from './flexbox/flexbox.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 
 @NgModule({
   declarations: [
-    ButtonsComponent
+    ButtonsComponent,
+    FlexboxComponent
   ],
   imports: [
     CommonModule,
     DemoRoutingModule,
-    MaterialModule
+    MaterialModule,
+    FlexLayoutModule
   ]
 })
 export class DemoModule { }
